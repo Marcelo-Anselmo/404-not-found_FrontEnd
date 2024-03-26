6903f97e49ba390121c6a95bd635d3d8a7ecf6c0
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-03-26 13:01:40 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Autentica Ulife</h1>
+        <img src="./public/Logo_404.png" alt="logo da 404" />
+      </div>
+    </>
+  );
+}
+
+export default App;
