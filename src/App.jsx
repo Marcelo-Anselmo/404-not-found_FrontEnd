ab9ec43a0be0a710a29395f7e8483c7e965aad54
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-03-28 19:21:30 -0300

    styles: Main_Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Link } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
+import Members from "./components/Members/Members.jsx";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <div>
+      <Header />
+      <section className="mainContent">
         <h1>Autentica Ulife</h1>
-        <img src="./public/Logo_404.png" alt="logo da 404" />
-      </div>
+        <div className="ataSection">
+          <button className="criaAta">
+            <Link to={""}>Criar ata</Link>
+          </button>
+        </div>
+      </section>
+      <Members></Members>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
@@ -10,7 +9,7 @@ const router = createBrowserRouter([
     element: <App />,
   },
   // {
-  //   path: "/login",
+  //   path: "/",
   //   element: <Login />,
   // },
 ]);
