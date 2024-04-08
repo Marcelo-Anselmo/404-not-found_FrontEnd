57887e7761273a0dc38d5cec4623e2ee3490c6a2
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-04-08 20:29:48 -0300

    styled:{Modal}

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import styles from "../Modal/Modal.module.scss";
+import "./index.scss";
 import { useForm } from "react-hook-form";
 import ReactModal from "react-modal";
 import * as yup from "yup";
@@ -7,94 +7,93 @@ import { useContext } from "react";
 import { DashboardContext } from "../../context/dashboard";
 
 const ModalSchema = yup.object({
-    nome: yup.string().required(),
-    email: yup.string().required(),
-    disciplina: yup.string().required(),
-    aula: yup.string().required(),
-    turno: yup.string().required(),
-    descricao: yup.string().notRequired(),
+  nome: yup.string().required(),
+  email: yup.string().required(),
+  disciplina: yup.string().required(),
+  aula: yup.string().required(),
+  turno: yup.string().required(),
+  descricao: yup.string().notRequired(),
 });
 
 const AtaModal = () => {
-    const { ataModal, setAtaModal, openAtaModal, closeAtaModal } =
-        useContext(DashboardContext);
+  const { ataModal, setAtaModal, openAtaModal, closeAtaModal } =
+    useContext(DashboardContext);
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({ resolver: yupResolver(ModalSchema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(ModalSchema) });
 
-    const onSubmitAtaModal = (data) => {
-        console.log(data);
-    };
+  const onSubmitAtaModal = (data) => {
+    console.log(data);
+  };
 
-    return (
-        <>
-            <ReactModal
-                isOpen={ataModal}
-                onRequestClose={() => closeAtaModal()}
-                className="modal"
-                overlayClassName="exterior-modal"
-            >
-                <div>
-                    <h1>Ata Online</h1>
-                    <button onClick={() => closeAtaModal()}>XX</button>
-                </div>
-                <form onSubmit={handleSubmit(onSubmitAtaModal)}>
-                    <label>Professor</label>
-                    <input
-                        type="text"
-                        placeholder="Roger S. Pressman"
-                        {...register("nome")}
-                    />
-                    <p>{errors.nome?.message}</p>
+  return (
+    <>
+      <ReactModal
+        isOpen={ataModal}
+        onRequestClose={() => closeAtaModal()}
+        className="Modal"
+        overlayClassName="exterior-modal">
+        <div className="closeBtn">
+          <button onClick={() => closeAtaModal()}>X</button>
+        </div>
+        <div>
+          <h1>Ata Online</h1>
+        </div>
+        <form onSubmit={handleSubmit(onSubmitAtaModal)}>
+          <label>Professor</label>
+          <input
+            type="text"
+            placeholder="Roger S. Pressman"
+            {...register("nome")}
+          />
+          <p>{errors.nome?.message}</p>
 
-                    <label>Email</label>
-                    <input
-                        type="text"
-                        placeholder="exemplo@email.com"
-                        {...register("email")}
-                    />
-                    <p>{errors.email?.message}</p>
+          <label>Email</label>
+          <input
+            type="text"
+            placeholder="exemplo@email.com"
+            {...register("email")}
+          />
+          <p>{errors.email?.message}</p>
 
-                    <label>Disciplina</label>
-                    <input
-                        type="text"
-                        placeholder="Modelagem de software"
-                        {...register("disciplina")}
-                    />
-                    <p>{errors.disciplina?.message}</p>
+          <label>Disciplina</label>
+          <input
+            type="text"
+            placeholder="Modelagem de software"
+            {...register("disciplina")}
+          />
+          <p>{errors.disciplina?.message}</p>
 
-                    <label>Turno</label>
-                    <input
-                        type="text"
-                        placeholder="Ex: noite"
-                        {...register("turno")}
-                    />
-                    <p>{errors.turno?.message}</p>
+          <label>Turno</label>
+          <input type="text" placeholder="Ex: noite" {...register("turno")} />
+          <p>{errors.turno?.message}</p>
 
-                    <label>Aula</label>
-                    <input
-                        type="number"
-                        placeholder="Aula: ex = 1"
-                        {...register("aula")}
-                    />
-                    <p>{errors.aula?.message}</p>
+          <label>Aula</label>
+          <input
+            type="number"
+            placeholder="Aula: ex = 1"
+            {...register("aula")}
+          />
+          <p>{errors.aula?.message}</p>
 
-                    <label>Descrição</label>
-                    <input
-                        type="text"
-                        placeholder="Descrição da aula"
-                        {...register("descricao")}
-                    />
-                    <p>{errors.descricao?.message}</p>
+          <label>Descrição</label>
+          <input
+            type="text"
+            placeholder="Descrição da aula"
+            {...register("descricao")}
+          />
+          <p>{errors.descricao?.message}</p>
 
-                    <button type="submit">Criar Ata</button>
-                </form>
-            </ReactModal>
-        </>
-    );
+          <div className="confirmBtn">
+            <button type="submit">Criar Ata</button>
+          </div>
+        </form>
+      </ReactModal>
+    </>
+  );
 };
 
 export default AtaModal;
